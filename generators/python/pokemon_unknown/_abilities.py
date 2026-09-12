f85c7bf2073671e4584a542f81a1036a6019e2d3
Garrick <garrickbrazil@gmail.com>
2026-09-12 14:19:09 -0400

    Pokemon unknown abilities list skips ability none / null

--- generators/python/pokemon_unknown/_abilities.py
+++ generators/python/pokemon_unknown/_abilities.py
@@ -42,30 +42,31 @@ def process():
             unordered[int(parts[2], 16)] = name
 
     ordered = collections.OrderedDict(sorted(unordered.items()))
-    _abilities = [None] * (max(ordered) + 1)
+    _abilities = [None] * max(ordered)
 
     for index, name in ordered.items():
+        slot = index - 1
         if name in _DISPLAY_OVERRIDES:
             display = _DISPLAY_OVERRIDES[name]
         else:
             display = None
 
         if display is not None and game_strings.has_ability(display):
-            _abilities[index] = "pokemon.ability." + game_strings.clean_up(display)
+            _abilities[slot] = "pokemon.ability." + game_strings.clean_up(display)
             continue
 
         lookup_key = name.lower().replace("_", "")
         if lookup_key in lookup:
-            _abilities[index] = "pokemon.ability." + lookup[lookup_key]
+            _abilities[slot] = "pokemon.ability." + lookup[lookup_key]
         else:
             if display is None:
                 display = " ".join(p[0].upper() + p[1:].lower() for p in name.split("_"))
                 print(f"\tWARNING: No Pokelink translation for {display}")
             key = _PREFIX + game_strings.clean_up(display)
             translations.add_translation(key, display)
-            _abilities[index] = key
+            _abilities[slot] = key
 
-        _ability_name_by_constant[name] = _abilities[index]
+        _ability_name_by_constant[name] = _abilities[slot]
 
 
 def generate():
